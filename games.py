a0ae3da48469f74e749458fdaedd718fd92547e1
SUS <konnorrk850@gmail.com>
2025-01-22 17:05:54 +0300

    Adding pages

--- games.py
+++ games.py
@@ -10,4 +10,24 @@ async def the_last_of_us(request: Request):
 
 @router.get("/the_last_of_us_2")
 async def the_last_of_us(request: Request):
-    return templates.TemplateResponse("The Last Of Us 2.html", {"request": request})
+    return templates.TemplateResponse("The_Last_of_Us_2.html", {"request": request})
+
+@router.get("/horizon_zero_dawn")
+async def the_last_of_us(request: Request):
+    return templates.TemplateResponse("Horizon_Zero_Dawn.html", {"request": request})
+
+@router.get("/horizon_forbidden_west")
+async def the_last_of_us(request: Request):
+    return templates.TemplateResponse("Horizon_Forbidden_West.html", {"request": request})
+
+@router.get("/mass_effect")
+async def the_last_of_us(request: Request):
+    return templates.TemplateResponse("Mass_Effect.html", {"request": request})
+
+@router.get("/mass_effect_2")
+async def the_last_of_us(request: Request):
+    return templates.TemplateResponse("Mass_Effect_2.html", {"request": request})
+
+@router.get("/mass_effect_3")
+async def the_last_of_us(request: Request):
+    return templates.TemplateResponse("Mass_Effect_3.html", {"request": request})
