76cd22fc735fa04bc97cdf7758e365691c8b28e4
SUS <konnorrk850@gmail.com>
2025-03-02 16:07:35 +0300

    Fix some pages

--- pages.py
+++ pages.py
@@ -1,12 +1,37 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from fastapi import Query
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
+def get_db_connection():
+    return psycopg2.connect(
+        dbname="InfoPages",
+        user="postgres",
+        password="Beripal826har",
+        host="127.0.0.1",
+        port="5432",
+        cursor_factory=RealDictCursor
+    )
+    
+def get_db():
+    conn = get_db_connection()
+    try:
+        yield conn  
+    finally:
+        conn.close()
+
 @router.get("/first_page")
-async def first_page(request: Request):
-    return templates.TemplateResponse("First_Page.html", {"request": request})
+async def first_page(request: Request, game_id: int = Query(...), db=Depends(get_db)):
+    cur = db.cursor()
+    cur.execute('SELECT id, name FROM "InfoPage"')
+    games = cur.fetchone()
+    cur.close()
+    game_list = [{"id": game[0], "name": game[1]} for game in games]
+    return templates.TemplateResponse("First_Page.html", {"request": request, "games": game_list})
 
 @router.get("/second_page")
 async def second_page(request: Request):
